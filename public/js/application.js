646cab0d2d5ddd11505daa5523adf98afeff064b
helin24 <helin.shiah@gmail.com>
2014-10-11 20:04:50 -0500

    pipe break and fix methods

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
+
+	var Pipe = function(id, htmlElement) {
+		this.broken = false;
+		this.pipeId = id;
+		this.$htmlElement = htmlElement;
+		this.break = function() {
+			this.$htmlElement.html("X");
+		};
+		this.repair = function() {
+			this.$htmlElement.html("_");
+		};
+
+	};
+
+	var pipesHTML = document.getElementsByClassName("pipe");
+
+	var pipes = [];
+
+	for(var i=0; i < pipesHTML.length; i++) {
+		var pipeId = pipesHTML[i].id;
+		var pipeNum = pipeId.substr(5,pipeId.length - 5);
+		var $pipeElement = $("#" + pipesHTML[i].id);
+		pipes.push(new Pipe(pipeNum, $pipeElement));
+	};
+	debugger;
+
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
